da98ad8aa930065f16fa9f2687eb6c146a055dd9
unknown <1660396419@qq.com>
2020-08-28 19:00:52 +0800

    打包配置

---
+++ build/webpack.dll.conf.js
@@ -0,0 +1,55 @@
+var path = require('path')
+var webpack = require('webpack')
+
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+    // 你想要打包的模块的数组
+    entry: {
+        vendor: [
+            'vue',
+            'lodash',
+            'vuex',
+            'axios',
+            'vue-router',
+            'iview',
+            'element-ui',
+            'echarts',
+            'xlsx',
+            'jquery',
+            'vue-fullcalendar',
+            'vue-cookie',
+            'handsontable',
+        ],
+    },
+    output: {
+        path: path.join(__dirname, '../dist/vendor-dll-js'), // 打包后文件输出的位置
+        filename: '[name].dll.js',
+        library: '[name]_library',
+        // vendor.dll.js中暴露出的全局变量名。
+        // 主要是给DllPlugin中的name使用，
+        // 故这里需要和webpack.DllPlugin中的`name: '[name]_library',`保持一致。
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            vue$: 'vue/dist/vue.esm.js',
+            '@': resolve('src'),
+        },
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(__dirname, '.', '[name]-manifest.json'),
+            name: '[name]_library',
+            context: __dirname,
+        }),
+        // 压缩打包的文件，与该文章主线无关
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+        }),
+    ],
+}
